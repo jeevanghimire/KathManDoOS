82b090072cd11cfafe29354504ffe72da2160e42
Jeevan Ghimire <imjeevanghimire@gmail.com>
2023-09-23 02:23:24 +0545

    Stepping Up

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,14 +1,24 @@
-org 0x7C00
+org 0x0
 bits 16
+
 
 %define ENDL 0x0D, 0x0A
 
+
 start:
-    jmp main
+    ; print hello world message
+    mov si, msg_hello
+    call puts
+
+.halt:
+    cli
+    hlt
+
 ;
-;Prints a string to the screen
-;params:
+; Prints a string to the screen
+; Params:
 ;   - ds:si points to string
+;
 puts:
     ; save registers we will modify
     push si
@@ -16,48 +26,20 @@ puts:
     push bx
 
 .loop:
-    lodsb                
-    or al, al             
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0E
-    mov bh, 0
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
 
     jmp .loop
 
-
-
- 
 .done:
     pop bx
     pop ax
-    pop si
-    ret 
+    pop si    
+    ret
 
-
-main:
-    ;setup data segements
-    mov ax,0
-    mov ds,ax
-    mov es,ax
-    
-    ; setup stack
-    mov ss,ax
-    mov sp, 0x7C00
-
-
-    ; print message
-    mov si, msg_hello
-    call puts
-
-    hlt 
-
-.halt:
-    jmp .halt
-
-    msg_hello: db  'Hello World!',ENDL,0
-
-times 510-($-$$) db 0
-dw 0AA55h
-
+msg_hello: db 'Hello world from KERNEL!', ENDL, 0
